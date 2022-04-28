090cebf76a744f01ddac5743ba2b3395f20f93a9
ValerioSpagnoli <99901284+ValerioSpagnoli@users.noreply.github.com>
2022-04-28 09:39:51 +0200

    form registrazione

---
+++ area personale/js/script.js
@@ -0,0 +1,38 @@
+function validaRegistrazione(){
+
+    //test cognome con lettera maiuscola
+
+    var cognome = document.registrazione.cognome.value;
+    console.log(cognome);
+    var test = null
+    for(var i=0; i<cognome.length; i++){
+        test= cognome.charAt(i);
+        console.log(test);
+        if(!isNaN(test)){
+            alert("Inserire solo caratteri alfabetici nel cognome");
+            return false;
+        }
+
+    }
+
+    var nome = document.registrazione.nome.value;
+    console.log(nome);
+    test = null
+    for(var i=0; i<nome.length; i++){
+        test= nome.charAt(i);
+        console.log(test);
+        if(!isNaN(test)){
+            alert("Inserire solo caratteri alfabetici nel nome");
+            return false;
+        }
+    }
+
+    alert("Registrazione avvenuta con successo!")
+
+
+}
+
+
+function validaLogin(){
+    
+}
