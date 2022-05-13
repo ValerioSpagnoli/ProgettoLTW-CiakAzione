45b3a36386581199f16c10c2d66a354dc7ec2e7d
ValerioSpagnoli <99901284+ValerioSpagnoli@users.noreply.github.com>
2022-05-13 10:59:37 +0200

    fix

--- i nostri cinema/js/script.js
+++ i nostri cinema/js/script.js
@@ -1,6 +1,6 @@
 function salvaCinema(titolo){
     var scadenza = new Date();
     var adesso = new Date();
-    scadenza.setTime(adesso.getTime() + (parseInt(1) * 5000));
+    scadenza.setTime(adesso.getTime() + (parseInt(1) * 86400000));
     document.cookie = "cinema=" + titolo + '; expires=' + scadenza.toGMTString() + '; path=/';
 }
